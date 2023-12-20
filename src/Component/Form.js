0f3223b3e67ec0c998baf6eebe06ad9d5264879b
unknown <mahsuma.rezai8@gmail.com>
2023-12-20 08:58:22 -0800

    last for 20 dec

--- src/Component/Form.js
+++ src/Component/Form.js
@@ -2,19 +2,28 @@
 import { Fragment } from 'react';
 import Card from './Card.js'
 import Head from './Head.js';
+import classes from './Form.module.css'
 const Form = (props) => {
     return (
         <Fragment>
             <Card>
-                <Head/>
-                <h1>hrllo</h1>
+                <Head />
+                <form>
+                    <div className={classes.inputs}>
+                        <input className={classes.valueinput} placeholder="Username" />
+
+                    </div>
+                   
+
+                </form>
+
 
             </Card>
-           
+
 
 
         </Fragment>
-    );
-};
-
-export default Form;
+            );
+        };
+        
+        export default Form;
